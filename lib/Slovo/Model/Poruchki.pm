cf9bede246a0075177aa6cc4cfa4b0bd4698a644
Красимир Беров <berov@cpan.org>
2022-02-07 00:20:30 +0200

    Поръчка с товарителница - работи!!!
    Следва: Тестове и  подготвяне на издание.

--- lib/Slovo/Model/Poruchki.pm
+++ lib/Slovo/Model/Poruchki.pm
@@ -5,9 +5,9 @@ my $table = 'orders';
 has table => $table;
 
 sub add ($m, $row) {
-  $row->{tstamp} //= time - 1;
-  $row->{created_at}  //= $row->{tstamp};
-  $m->c->debug($row);
-  return $m->next::method($row);
+    $row->{tstamp}     //= time - 1;
+    $row->{created_at} //= $row->{tstamp};
+    $m->c->debug($row);
+    return $m->next::method($row);
 }
 1;
